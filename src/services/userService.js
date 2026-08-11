9ba3bb2dc4539bcf88e486771e31a35320d5f6ef
SonVoo <svo70458@gmail.com>
2026-08-11 15:41:59 +0700

    Display data on the screen

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,10 @@ import axios from "../axios";
 const handleLoginApi = (userEmail, userPassword) => {
     return axios.post('/api/login', { email: userEmail, password: userPassword });
 }
+const getAllUsers = (inputID) => {
+    return axios.post('/api/get-all-users', { id: inputID })
+}
 export {
-    handleLoginApi
+    handleLoginApi, getAllUsers
 }
 
