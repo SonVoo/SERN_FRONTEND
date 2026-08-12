062f8404de579e0b737e2c93394f08de64f341ac
SonVoo <svo70458@gmail.com>
2026-08-12 10:49:59 +0700

    Add-New-Users

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers } from '../../services/userService';
+import { getAllUsers, createNewUserService } from '../../services/userService';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         {
             this.state = {
-                arrUsers: []
+                arrUsers: [],
+                isOpenModalUser: false,
             }
         }
     }
 
     async componentDidMount() {
+        await this.getAllUsersFromReact();
+    }
+    getAllUsersFromReact = async () => {
         let response = await getAllUsers('ALL');
         if (response && response.errCode === 0) {
             this.setState({
@@ -22,13 +28,48 @@ class UserManage extends Component {
             })
         }
     }
+    handleAddNewUser = async () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
 
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUserService(data);
+            if (response && response.errCode !== 0) {
+                alert(response.errMessage)
+            } else {
+                await this.getAllUsersFromReact();
+                this.setState({
+                    isOpenModalUser: false
+                })
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     render() {
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    createNewUser={this.createNewUser}
+                />
                 <div className='title text-center'>Manages users</div>
+                <div className="mx-1">
+                    <button className="btn btn-primary px-3" onClick={() => this.handleAddNewUser()}>
+                        <i className="fas fa-plus"></i> Add new user
+                    </button>
+                </div>
                 <div className='users-table mt-3 mx-1'>
                     <table>
                         <thead>
@@ -52,19 +93,17 @@ class UserManage extends Component {
                                             <button className='btn-edit'><i className='fas fa-pencil-alt'></i>Edit</button>
                                             <button className='btn-delete'><i className='fas fa-trash'></i>Delete</button>
                                         </td>
-
                                     </tr>
                                 )
                             })}
-
                         </tbody>
                     </table>
                 </div>
             </div>
-        );
+        )
     }
-
 }
+
 
 const mapStateToProps = state => {
     return {
@@ -75,5 +114,6 @@ const mapDispatchToProps = dispatch => {
     return {
     };
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -6,7 +6,22 @@ const handleLoginApi = (userEmail, userPassword) => {
 const getAllUsers = (inputID) => {
     return axios.post('/api/get-all-users', { id: inputID })
 }
+const handleCreateNewUser = (data) => {
+    return axios.post('/api/create-new-user', {
+        email: data.email,
+        password: data.password,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phonenumber: data.phonenumber,
+        gender: data.gender,
+        roleId: data.roleId
+    })
+}
+const createNewUserService = (data) => {
+    return axios.post('/api/create-new-user', data);
+}
 export {
-    handleLoginApi, getAllUsers
+    handleLoginApi, getAllUsers, createNewUserService
 }
 
